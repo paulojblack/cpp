e843b33608015d916d49ea383415fad13e2e74cd
Paulo Black <pblack@goodwaygroup.com>
2022-02-14 15:59:12 -0500

    lrn2map

---
+++ stan_106b/hw2/map.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+char MostFrequentCharacter(ifstream &file, int &numOccurrences);
+
+int main() {
+    ifstream file;
+    
+    int numOccurences;
+    char mostOccuredChar = MostFrequentCharacter(file, numOccurences);
+}
+
+char MostFrequentCharacter(ifstream &file, int &numOccurrences) {
+    map<char, int> butts;
+    string line;
+    file.open("./map.txt");
+    while(getline(file, line)) {
+        for(int i = 0; i < line.length(); i++) {
+            cout << line[i] << endl;
+        }
+        cout << line << endl;
+    }
+    butts.insert(std::pair<char, int>('a', 100));
+    cout << butts.at('b') << endl;
+    return 'a';
+}
